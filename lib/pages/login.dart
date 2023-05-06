08dfa3c59e5f55f9461d4f296139a1f1a2f309cd
David Mezza <d.mezza@zunocarbon.com>
2023-05-06 19:58:38 +0300

    :sparkles: To be continued - Login Page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -8,14 +8,108 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool _isObscure = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("Login Page")),
-        body: ListView(
-          children: const [
-            Text("LOGIN"),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const FlutterLogo(size: 100),
+            const SizedBox(height: 50),
+            _buildTextField("Email"),
+            const SizedBox(height: 20),
+            _buildPasswordField("Password"),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: () {},
+              child: const Text("Forgot Password?"),
+              style: TextButton.styleFrom(primary: Colors.grey),
+            ),
+            // const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("LOGIN"),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                textStyle: const TextStyle(fontSize: 20),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text("Or login with"),
+            const SizedBox(height: 20),
+            _buildGoogleSignInButton(),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text("New to Clinic?"),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text("Register"),
+                  style: TextButton.styleFrom(primary: Colors.blue),
+                ),
+              ],
+            ),
           ],
-        ));
+        )));
+  }
+
+  Widget _buildTextField(String label) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildGoogleSignInButton() {
+    return OutlinedButton(
+      onPressed: () {},
+      style: OutlinedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        side: const BorderSide(color: Colors.grey),
+        padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: const <Widget>[
+          // Image(image: AssetImage("assets/images/google_logo.png"), height: 30),
+          SizedBox(width: 10),
+          Text("Google"),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPasswordField(String label) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      child: TextField(
+        obscureText: _isObscure,
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(),
+          suffixIcon: IconButton(
+            icon: _isObscure
+                ? const Icon(Icons.visibility_off)
+                : const Icon(Icons.visibility),
+            onPressed: () {
+              setState(() {
+                _isObscure = !_isObscure;
+              });
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
